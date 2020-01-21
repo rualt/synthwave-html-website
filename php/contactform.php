1bb78c73859211207cc562c65b58669a23001812
rualt <rualnar@gmail.com>
2020-01-22 00:36:32 +0400

    fix redirection

--- php/contactform.php
+++ php/contactform.php
@@ -1,16 +1,17 @@
 <?php
 
-if(isset($_POST['submit'])) {
+
+if (isset($_POST['submit'])) {
         $name = $_POST['name'];
         $mailFrom = $_POST['email'];
         $message = $_POST['message'];
-
         $mailTo = "rurgar@mail.ru";
         $subjest = "Synthwave website message from" . $name;
         $headers = "From: ".$mailFrom;
         $txt = "You have recieved an email from ".$name.".\n\n".$message;
 
-
         mail($mailTo, $subjest, $txt, $headers);
-        header("Location: index.php?mailsend");
+        header("Location: ".$_SERVER['HTTP_REFERER']);
+} else {
+    header("Location: /index.html");
 }
